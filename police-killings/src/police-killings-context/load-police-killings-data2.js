6e17fdfe99a7de9143a8b7727c05342e7cfa80b1
schroederarthur <schroederarthur1998@hotmail.com>
2020-07-20 22:53:54 +0200

    moved csv file into public folder

--- police-killings/src/police-killings-context/load-police-killings-data2.js
+++ police-killings/src/police-killings-context/load-police-killings-data2.js
@@ -1,7 +1,7 @@
 import parse from 'csv-parse/lib/sync'
 
 export function loadPoliceKillingsData() {
-    return fetch('https://raw.githubusercontent.com/fivethirtyeight/data/master/police-killings/police_killings.csv')
+    return fetch('../../public/police_killings.csv')
         .then((response) => {
             return response.text();
         })
--- police-killings/src/police-killings-context/police-killings-context.test.js
+++ police-killings/src/police-killings-context/police-killings-context.test.js
@@ -3,7 +3,7 @@ import PoliceKillingsData from './load-police-killings-data2'
 
 describe('loadPoliceKillingsData', () => {
     it('should provide police_killings.csv as json promise', async () => {
-        const csvContent = readFileSync(__dirname +'../police_killings.csv', 'utf8');
+        const csvContent = readFileSync(__dirname +'../../public/police_killings.csv', 'utf8');
         const rows = csvContent.split('\r')
         const labels = rows[0].split(',')
         fetch.mockResponseOnce(csvContent)
